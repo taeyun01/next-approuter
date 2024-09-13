91b080c31b556583dcda45fa09dd60771fecbd18
taeyun <lwe12f675@gmail.com>
2024-09-13 16:26:48 +0900

    feat: 공통 레이아웃 설정

---
+++ src/app/(with-searchbar)/layout.tsx
@@ -0,0 +1,10 @@
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      윗-서치바 레이아웃 폴더에 있는 임시 서치바
+      <div>{children}</div>
+    </div>
+  );
+};
+
+export default Layout;
