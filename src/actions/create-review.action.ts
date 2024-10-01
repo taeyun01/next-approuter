5375096263840d026a7aeeaabecc92f4d4a91b27
taeyun <lwe12f675@gmail.com>
2024-10-01 18:54:59 +0900

    feat: 클라이언트 컴포넌트에서 서버액션 (useActionState()를 활용해 form태그의 로딩 상태, 중복 제출방지, 에러 핸들링 처리 )

--- src/actions/create-review.action.ts
+++ src/actions/create-review.action.ts
@@ -1,11 +1,16 @@
 "use server";
+import { delay } from "@/util/delay";
 import { revalidatePath, revalidateTag } from "next/cache";
 
 // 별도의 파일로 분리했을 때는 함수 안쪽이 아닌 최상단에 작성하는게 일반적임
 
 //? 서버액션을 사용하는 이유는 조금더 간결하고 편리하게 서버측에서 실행되는 어떠한 동작을 정의하는데 있다.
 //* 1. 서버액션을 만들면
-export const createReviewAction = async (formData: FormData) => {
+export const createReviewAction = async (
+  _: any, // 해당 방식으로도 쓴다.
+  // state: any,
+  formData: FormData
+) => {
   // "use server";
   //* 2. 자동으로 ⬇️아래 코드를 실행하는 API가 하나 자동으로 생성된다.
   //* 3. 그런 API는 브라우저에서 form태그를 제출했을 때 자동으로 호출이 된다.
@@ -16,10 +21,15 @@ export const createReviewAction = async (formData: FormData) => {
 
   // 클라이언트랑 서버랑 같이 예외처리 해주는 이유 : 서로 100% 믿을 수 없음
   if (!content || !author || !bookId) {
-    return; // 빈값 입력 시 서버액션 실행 x
+    //* 해당 리턴값이 ReviewEditor컴포넌트의 useActionState()에 state값으로 들어간다
+    return {
+      status: false,
+      error: "리뷰내용과 작성자를 입력해주세요",
+    };
   }
 
   try {
+    await delay(1500);
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`,
       {
@@ -28,24 +38,20 @@ export const createReviewAction = async (formData: FormData) => {
       }
     );
 
-    console.log(response.status);
-    //* 결론적으로 revalidatePath를 사용해 /book/${bookId} 해당 페이지를 다시 렌더링 해줘서 실시간으로 댓글이 작성되는 걸 볼 수 있다.
-    // 1. 특정 주소의 해당하는 페이지만 재검증
-    // revalidatePath(`/book/${bookId}`); // book/page.tsx 컴포넌트가 다시 렌더링됨, 자식 컴포넌트도 전부 재렌더링 (DetailBooks, ReviewEditor, ReviewList) 자식 컴포넌트에 있는 컴포넌트 들도 fetch를 통해 데이터를 불러오는데 이것도 다시 실행됨 (서버측에서만 호출할 수 있다. 페이지 전체가 재생성 되지 때문에 캐시 옵션을 줘도 캐시도 무효화(삭제)된다. 풀라우트 캐시까지 삭제됨, 다시 서버측에서 페이지를 생성해 줘서 업데이트함)
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
 
-    // 2. 특정 경로의 모든 동적 페이지를 재검증 (첫번째 인수는 url주소 경로가 아닌 해당 페이지 컴포넌트가 작성된 폴더 또는 파일의 경로를 적어줌)
-    // revalidatePath("/book/[id]", "page");
-
-    // 3. 특정 레이아웃을 갖는 모든 페이지 재검증 ((with-searchbar)에 있는 layout.tsx을 기준으로 재검증 하고 싶을 때 )
-    // revalidatePath("(with-searchbar)", "layout");
-
-    // 4. 모든 데이터 재검증 (모든 페이지 재검증) ("/" index경로에 있는 레이아웃인 루트 레이아웃이 전부다 재검증이 이루어짐)
-    // revalidatePath("/", "layout");
-
-    // 5. 태그 기준. 데이터 캐시 재검증 (데이터 캐시의 태그란? => next: {tags: [`review-${bookId}]} 해당 태그를 갖는 데이터 패칭만 다시 렌더링된다. )
     revalidateTag(`review-${bookId}`); // 제일 효율적 오직 해당 태그값을 갖는 fetch메서드의 데이터 페칭만 삭제가 된다.
+    // 성공시
+    return {
+      status: true,
+      error: "",
+    };
   } catch (error) {
-    console.error(error);
-    return;
+    return {
+      status: false,
+      error: `리뷰 작성에 실패했습니다. ${error}`,
+    };
   }
 };
--- src/components/review-editor.tsx
+++ src/components/review-editor.tsx
@@ -1,16 +1,50 @@
+"use client";
+
 import style from "./review-editor.module.css";
 import { createReviewAction } from "@/actions/create-review.action";
+import { useActionState, useEffect } from "react";
 
+//* 클라이언트 컴포넌트에서의 서버액션
+//* ReviewEditor를 클라이언트 컴포넌트로 전환하여 useActionState()를 활용해 form에 로딩 상태, 중복 제출방지, 에러 핸들링 처리하기
+//* from태그를 사용할 경우 되도록이면 클라이언트 컴포넌트로 하고 useActionState()를 적극적으로 활용해라 (from은 중복제출, 로딩 상태, 에러 핸들링이 거의 필수기 때문)
 const ReviewEditor = ({ bookId }: { bookId: string }) => {
+  // useActionState()의 첫 번째 인수는 핸들링하려는 서버액션 함수, 두 번째는 상태의 초기값
+  // state값은 서버액션이 실행되어 성공 했을때나 실패했을때 리턴값으로 담기게 된다.
+  const [state, fromAction, isPending] = useActionState(
+    createReviewAction,
+    null
+  );
+
+  // state값이 바뀔 때 에러가 발생했는지 안했는지 검증
+  useEffect(() => {
+    // state.status가 false면 실행
+    if (state && !state.status) {
+      alert(state.error);
+    }
+  }, [state]);
+
   return (
     <section>
-      <form className={style.form_container} action={createReviewAction}>
+      <form className={style.form_container} action={fromAction}>
         {/* 대부분의 데이터를 서버액션으로 충분히 넘길 수 있으니까 해당 트릭을 잘 이용하면 된다 */}
         <input name="bookId" value={bookId} type="hidden" />
-        <textarea required name="content" placeholder="리뷰내용" />
+        <textarea
+          disabled={isPending}
+          required
+          name="content"
+          placeholder="리뷰내용"
+        />
         <div className={style.submit_container}>
-          <input required name="author" placeholder="작성자" type="text" />
-          <button type="submit">작성하기</button>
+          <input
+            disabled={isPending}
+            required
+            name="author"
+            placeholder="작성자"
+            type="text"
+          />
+          <button disabled={isPending} type="submit">
+            {isPending ? "작성중..." : "작성하기"}
+          </button>
         </div>
       </form>
     </section>
